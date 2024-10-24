fdc3599c7e9e16a1d5e13102dc4b911880dfc879
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-24 09:22:52 +0200

    chore: environment setup

---
+++ webpack.config.cjs
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './dev/index.ts', 
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/, 
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
