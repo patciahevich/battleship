e0adb46a36c0d13d346b5dd63499925ee7b9fdef
Volha Patsiahevich <zaprudskaya@gmail.com>
2024-10-28 13:53:05 +0100

    fix: fix bot ships

--- dev/game/randomShips.ts
+++ dev/game/randomShips.ts
@@ -3,43 +3,61 @@ import Ship from '../ships/ship';
 
 const botShips: ShipData[] = [
   {
-    position: { x: 0, y: 0 },
+    position: { x: 1, y: 1 },
     direction: true,
     length: 4,
     type: 'huge',
   },
   {
-    position: { x: 2, y: 2 },
+    position: { x: 4, y: 1 },
     direction: false,
     length: 3,
     type: 'large',
   },
   {
-    position: { x: 5, y: 5 },
+    position: { x: 8, y: 3 },
     direction: true,
     length: 3,
     type: 'large',
   },
   {
-    position: { x: 7, y: 1 },
+    position: { x: 5, y: 3 },
     direction: true,
     length: 2,
     type: 'medium',
   },
   {
-    position: { x: 4, y: 7 },
+    position: { x: 3, y: 6 },
     direction: false,
     length: 2,
     type: 'medium',
   },
   {
-    position: { x: 9, y: 0 },
+    position: { x: 1, y: 8 },
+    direction: false,
+    length: 2,
+    type: 'medium',
+  },
+  {
+    position: { x: 8, y: 1 },
     direction: true,
     length: 1,
     type: 'small',
   },
   {
-    position: { x: 1, y: 9 },
+    position: { x: 3, y: 3 },
+    direction: false,
+    length: 1,
+    type: 'small',
+  },
+  {
+    position: { x: 7, y: 7 },
+    direction: false,
+    length: 1,
+    type: 'small',
+  },
+  {
+    position: { x: 5, y: 8 },
     direction: false,
     length: 1,
     type: 'small',
